3cff70091322dc365f7879ddb2eec501ff871746
jcyoshida <jcyoshida@msn.com>
2016-03-08 15:26:52 -0800

    added a plan c.

---
+++ spip/planc.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="planc.aspx.cs" Inherits="spip.planc" MasterPageFile="~/Site1.Master" %>
+
+<asp:Content ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    
+    <form runat="server">
+        <fieldset disabled>
+            <div class="form-group">
+                <asp:label for="disabledTextInput" id="lbldti" runat="server"></asp:label>
+                <asp:TextBox ID="disabledTextInput" class="form-control" runat="server"></asp:TextBox>
+            </div>
+            <div class="form-group">
+                <asp:label for="disabledTextInput2" id="lbldti2" runat="server"></asp:label>
+                <asp:TextBox ID="disabledTextInput2" class="form-control" Rows="5" runat="server"></asp:TextBox>
+            </div>
+        </fieldset>
+    <h4 class="text-left">Please Select a Strategy</h4>
+
+                <asp:GridView id="mygv" runat="server" CssClass="table table-hover table-striped" OnRowDataBound="mygv_RowDataBound" AutoGenerateColumns="False" GridLines="None">
+                <Columns>
+                    <asp:BoundField DataField="id" HeaderText="Strategy" />
+                    <asp:BoundField DataField="stratDesc" HeaderText="Description" />
+                </Columns>
+                <RowStyle CssClass="click-row" />
+            </asp:GridView>
+        </form>
+    <asp:Label ID="lblInfo" runat="server" Text="Label" Visible="false"></asp:Label>
+                    <script>
+            $(".click-row").click(function () {
+                window.document.location = $(this).data("href");
+            });
+        </script>
+
+</asp:Content>
---
+++ spip/planc.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Web.Configuration;
+
+
+namespace spip
+{
+    public partial class planc : System.Web.UI.Page
+    {
+        private string connectionString = WebConfigurationManager.ConnectionStrings["strategic_planConnectionString"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string goal = Request.QueryString["g"];
+            string obj = Request.QueryString["o"];
+            lbldti.Text = "<strong>GOAL " + goal + ":</strong>";
+            lbldti2.Text = "<strong>OBJECTIVE " + obj + ":</strong>";
+
+            MySqlConnection myConnection = new MySqlConnection(connectionString);
+            string selectSQL = "SELECT title FROM goal WHERE id='" + goal + "'";
+            string selectSQL2 = "SELECT objDesc FROM objective WHERE id='" + obj + "'";
+            MySqlCommand myCommand = new MySqlCommand(selectSQL, myConnection);
+            MySqlCommand cmd2 = new MySqlCommand(selectSQL2, myConnection);
+            string selectSQL3 = "SELECT * FROM strategy WHERE objID='" + obj + "'";
+            MySqlDataAdapter adapter = new MySqlDataAdapter(selectSQL3, myConnection);
+            DataSet ds = new DataSet();
+
+            try
+            {
+                myConnection.Open();
+                MySqlDataReader reader;
+                reader = myCommand.ExecuteReader();
+                reader.Read();
+                disabledTextInput.Text = reader["title"].ToString();
+                reader.Close();
+                reader = cmd2.ExecuteReader();
+                reader.Read();
+                disabledTextInput2.Text = reader["objDesc"].ToString();
+                reader.Close();
+
+                adapter.Fill(ds, "Strategies");
+                mygv.DataSource = ds;
+                mygv.DataBind();
+
+                //lblInfo.Text = "Server Ver." + myConnection.ServerVersion;
+
+            }
+            catch (Exception err)
+            {
+                lblInfo.Text = "error reading the db.";
+                lblInfo.Text += err.Message;
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+        }
+        protected void mygv_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            string goal = Request.QueryString["g"];
+            string obj = Request.QueryString["o"];
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                e.Row.Attributes["onmouseover"] = "this.style.cursor='pointer';";
+
+                int g = (int)DataBinder.Eval(e.Row.DataItem, "id");
+                e.Row.Attributes["data-href"] = "ap.aspx?g=" + goal + "&o=" + obj + "&s=" +g;
+            }
+        }
+    }
+}
